267ac827d512ab8c0cc444f26f70a53d16d521fe
Rupika Gupta <rupi.1788@gmail.com>
2021-01-06 16:55:03 -0800

    con-7 code to control conversion from Ounce to Grams

--- src/main/java/SecondConverter.java
+++ src/main/java/SecondConverter.java
@@ -1,4 +1,6 @@
 public class SecondConverter {
 
     //2nd converter is working and ready to be merged to master
+    //code to convert Ounce into Grams
+    //each unit Ounce is equal to 28.349units of Grams
 }
